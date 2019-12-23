83db9eb83d227874a4048ef312734e53ff6b5c0c
enamelizer <5801754+enamelizer@users.noreply.github.com>
2019-12-23 15:54:31 -0800

    Update copyright

--- VS_Solution/HrmHaystack/Properties/AssemblyInfo.cs
+++ VS_Solution/HrmHaystack/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HrmHaystack")]
-[assembly: AssemblyCopyright("Copyright © 2013 hermes-jr, Aaron DeMarre")]
+[assembly: AssemblyCopyright("Copyright © 2013 hermes-jr, enamelizer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
